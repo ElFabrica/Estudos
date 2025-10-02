452c926a94508168c29f65fb42b0e644e55d04c4
Arthur Fabricyo <142946955+ElFabrica@users.noreply.github.com>
2025-10-02 23:18:25 +0000

    finalização do sisteminha

--- app_calculos/src/App.tsx
+++ app_calculos/src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   const [currentX, setCurrentX] = useState("");
   const [currentY, setCurrentY] = useState("");
 
+  const [aprendization, setAprendization] = useState("")
   const [w0, setW0] = useState("");
   const [w1, setW1] = useState("");
   const [x, setX] = useState<number[]>([]);
@@ -12,9 +13,9 @@ function App() {
   const [prev, setPrev] = useState<number[] >([]);
   const [error, setError] = useState<number[]>([]);
   const [sumError, setSumError] = useState<number>(0);
-  const [sumErrorWithCoeficiente, setSumErrorWithCoeficiente] = useState<number | null>();
-  const [newW0, setNewW1] = useState<number | null>();
-  const [newW1, setNewW2] = useState<number | null>();
+  const [sumErrorWithCoeficiente, setSumErrorWithCoeficiente] = useState<number>(0);
+  const [newW0, setNewW0] = useState<number>();
+  const [newW1, setNewW1] = useState<number>();
   
   function handleAddX(){
     const numberX = Number(currentX)
@@ -42,8 +43,26 @@ function App() {
      
 
     //sum error
-    setSumError(newErrors.reduce((acc,current) => acc + current ,0))
+    const SumError = newErrors.reduce((acc,current) => acc + current ,0)
+    setSumError(SumError)
+
     //sum error with coeficiente
+    console.log(error)
+    const NewSumErrorWithCoeficiente = newErrors.reduce((acc, error, index) => (error * x[index]) + acc, 0);
+    
+    console.log(NewSumErrorWithCoeficiente)
+
+    setSumErrorWithCoeficiente(Number(NewSumErrorWithCoeficiente.toFixed(2)))
+  
+
+    // New coeficiente w0
+
+    const NewValueW0 = newW0 + Number(aprendization) * sumError
+    setNewW0(NewValueW0)
+    const NewValueW1 = newW1 + Number(aprendization) * NewSumErrorWithCoeficiente
+    setNewW1(NewValueW1)
+
+
   }
 
 
@@ -71,11 +90,12 @@ function App() {
 
           <button 
           className='bg-gray-400 rounded-sm text-white p-2'
-          onClick={handleAddY}> Adicionar X
+          onClick={handleAddY}> Adicionar y
           </button>
           </div>
 
          <div className='flex flex-col gap-2'>
+
          Adicione os coeficientes W0: 
           <input type="text" className='border-1 border-gray-400 rounded-md' value={w0} 
           onChange={(e) => setW0(e.target.value) }
@@ -86,19 +106,29 @@ function App() {
           <input type="text" className='border-1 border-gray-400 rounded-md' value={w1} 
           onChange={(e) => setW1(e.target.value) }
           />
+         Adicione os a taxa de aprendizagem: 
+
+        
+          <input type="text" className='border-1 border-gray-400 rounded-md' value={aprendization} 
+          onChange={(e) => setAprendization(e.target.value) }
+          />
 
          <button 
           className='bg-gray-400 rounded-sm text-white p-3'
           onClick={handleCalculateErrors}> Calcular coeficientes
           </button>
           <div>
-          <p>W1: {w0}</p>
-          <p>W0: {w1}</p>
+          <p>W0: {w0}</p>
+          <p>W1: {w1}</p>
+          <p>Aprendizagem: {aprendization}</p>
           <p>X: [{x.join(",")} ]</p>
           <p>Y: [{y.join(",")} ]</p>
         <p>Previsões: {prev.join(",")}</p>
         <p>Erros: {error.join(",")}</p>
         <p>Soma dos erros: {sumError}</p>
+        <p>Soma com os coeficientes: {sumErrorWithCoeficiente}</p>
+        <p>Nova interação W0: {newW0}</p>
+        <p>Nova interação W1: {newW1}</p>
         </div>
       </div>
       </div>
